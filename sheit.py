7567aebf0761aa4cb1600c6b5805dcf9847ec889
Ryan McKeever <rkfmckee@gmail.com>
2024-02-11 11:15:40 +0000

    Update for new Discord API version, and add requirements.txt

--- sheit.py
+++ sheit.py
@@ -1,6 +1,7 @@
 import discord, os, re, asyncio, pprint
+from discord import Intents
+from discord.ext import commands
 from dotenv import load_dotenv
-from discord.ext import commands, tasks
 from urllib.request import urlopen, Request, HTTPError, urlretrieve
 from bs4 import BeautifulSoup as bSoup
 
@@ -13,11 +14,15 @@ help_command = commands.DefaultHelpCommand(
     no_category = 'Sheit Bot Commands'
 )
 
+intents = Intents.default()
+intents.message_content = True
+
 bot = commands.Bot(
     command_prefix = "/sheit ",
     description = "SheitBot, a Netflix (un)original Bot which doesn't really need to exist.",
     help_command = help_command,
-    case_insensitive=True
+    case_insensitive=True,
+    intents=intents
 )
 
 filmNamePattern = re.compile('^"(.+)".*:')
